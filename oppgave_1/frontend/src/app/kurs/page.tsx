93eeb07a8b4a6c0c12e21d2f0acf19b33c5153cb
LegendDev <legenddevcode@gmail.com>
2024-12-03 18:51:37 +0100

    Update page.tsx
    legge til routing for lettere navigering

--- oppgave_1/frontend/src/app/kurs/page.tsx
+++ oppgave_1/frontend/src/app/kurs/page.tsx
@@ -2,11 +2,22 @@
 
 import Courses from "../../components/Courses";
 import All from "../../pages/All";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Course from "../../components/Course";
+import Lesson from "../../components/Lesson";
 
 export default function Pages() {
-    return (
-      <All>
-        <Courses />
-      </All>
-    );
+  return (
+    <main className="h-full">
+      <Router>
+        <All>
+          <Routes>
+            <Route path="/kurs" element={<Courses />} />
+            <Route path="/kurs/:courseSlug" element={<Course />} />
+            <Route path="/kurs/:courseSlug/:lessonSlug" element={<Lesson />} />
+          </Routes>
+        </All>
+      </Router>
+    </main>
+  );
   }
